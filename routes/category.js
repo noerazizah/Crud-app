fc4dfcf63e9edbb222f2dccd3af93ddd16127544
annisaraihana <annisaraihana@mail.ugm.ac.id>
2022-09-27 13:32:15 +0700

    menambahkan fitur GET category

--- routes/category.js
+++ routes/category.js
@@ -10,3 +10,12 @@ router.post("/", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get("/", async (req, res) => {
+    try {
+        const cats = await Category.find();
+        res.status(200).json(cats);
+    } catch (err) {
+        res.status(500),json(err);
+    }
+});
