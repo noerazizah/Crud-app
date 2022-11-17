0e1185b9f834aa96bdf6c4896d99a8e2f06f9dd8
Ilham Faizal Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2022-11-17 11:07:29 +0700

    feat(navbar):create navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      lookist
-    </div>
+    <NavBar />
   );
 }
 
