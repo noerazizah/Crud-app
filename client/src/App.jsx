695093011fb2472d9eb89a7774f0024c7740f869
elzanataliw <elza.natali.w@mail.ugm.ac.id>
2022-11-17 20:55:36 +0700

    fix(assets)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import Header from "./components/header/Header";
+// import Header from "./components/header/Header";
 import NavBar from "./components/navbar/NavBar";
-import Sidebar from "./components/sidebar/Sidebar";
-import Post from "./components/post/Post";
+// import Sidebar from "./components/sidebar/Sidebar";
+// import Post from "./components/post/Post";
 import Home from "./pages/home/Home";
+// import Login from "./components/pages/login/Login";
 
 function App() {
   return (
