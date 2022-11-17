b9192bc3680d5403b424506de4a5a86758de1620
resiyandawafa02 <75015733+reiiiofsunshine1@users.noreply.github.com>
2022-11-18 00:03:14 +0700

    Added Settings Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import NavBar from './components/navbar/NavBar';
 /*import NavBar from './components/navbar/NavBar';*/
 import SinglePost from './components/single-post/Singlepost';
+import Settings from './components/Settings/Settings';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         
       </div>
       <div className="App">
-        <SinglePost/>
+        <Settings/>
       </div>
     </header>
     
