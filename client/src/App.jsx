cd364cd7bd815b8206fd5a1655aeac267612ff1e
Ilham Faizal Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2022-11-17 11:36:13 +0700

    fix(conflict):fixing some conflict in package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,8 @@
-<<<<<<< HEAD:client/src/App.js
 import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <NavBar />
-=======
-import React from 'react';
-import Header from './components/header/Header';
-
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
->>>>>>> main:client/src/App.jsx
   );
 }
 
