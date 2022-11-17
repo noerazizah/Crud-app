d8b661a49307af646350260961d5e3e382647cae
elzanataliw <elza.natali.w@mail.ugm.ac.id>
2022-11-17 23:10:53 +0700

    feat(login):create login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 // import Header from "./components/header/Header";
-import NavBar from "./components/navbar/NavBar";
+// import NavBar from "./components/navbar/NavBar";
 // import Sidebar from "./components/sidebar/Sidebar";
 // import Post from "./components/post/Post";
-import Home from "./pages/home/Home";
-// import Login from "./components/pages/login/Login";
+// import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Home />
+      {/* <NavBar />
+      <Home /> */}
+      <Login/>
     </>
   );
 }
