34e91b7095d2b3e2e482adfd27e50a3d72ca6950
annisaraihana <annisaraihana@mail.ugm.ac.id>
2022-11-17 17:47:16 +0700

    feat(sidebar):edited sidebar component

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -19,51 +19,6 @@ export default function Sidebar() {
           amet ex esse.Sunt eu ut nostrud id quis proident.
         </p>
       </div>
-
-      {/* <div className="sidebarItem">
-        <span className="sidebarTitle">CATEGORIES</span>
-        <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <a className="link" to="/posts?cat=Life">
-              Life
-            </a>
-          </li>
-          <li className="sidebarListItem">
-            <a className="link" to="/posts?cat=Music">
-              Music
-            </a>
-          </li>
-          <li className="sidebarListItem">
-            <a className="link" to="/posts?cat=Sport">
-              Sport
-            </a>
-          </li>
-          <li className="sidebarListItem">
-            <a className="link" to="/posts?cat=Style">
-              Style
-            </a>
-          </li>
-          <li className="sidebarListItem">
-            <a className="link" to="/posts?cat=Tech">
-              Tech
-            </a>
-          </li>
-          <li className="sidebarListItem">
-            <a className="link" to="/posts?cat=Cinema">
-              Cinema
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="sidebarItem">
-        <span className="sidebarTitle">FOLLOW US</span>
-        <div className="sidebarSocial">
-          <i className="sidebarIcon fab fa-facebook-square"></i>
-          <i className="sidebarIcon fab fa-instagram-square"></i>
-          <i className="sidebarIcon fab fa-pinterest-square"></i>
-          <i className="sidebarIcon fab fa-twitter-square"></i>
-        </div>
-      </div> */}
     </div>
     </div>
   );
