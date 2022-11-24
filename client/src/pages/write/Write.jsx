81243a2cf58f048216a53c54f04411dd9511ce90
noerazizah <noerazizah2020@mail.ugm.ac.id>
2022-11-24 15:44:33 +0700

    feat(write): create write page

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,112 +1,75 @@
-<<<<<<< HEAD
-import React from "react";
-import Imgg from "../../assets/6221846 1.png";
-import Imgee from "../../assets/Vector.png";
-import "./Write.css";
-
-export default function Write() {
-  return (
-    <div class="write">
-        <img
-        className="writeImg"
-        src={Imgg}
-        alt=""
-      />
-      <form className="writeForm">
-        <div class="flex-container">
-            <img className="Iconadd"
-            src={Imgee}
-            alt=""
-        />
-          <input id="fileInput" type="file" style={{ display: "none" }} />
-          <input
-            className="writeInput"
-            placeholder="Title"
-            type="text"
-            autoFocus={true}
-          />
-        </div>
-        <div className="writeFormGroup">
-          <textarea
-            className="writeInput writeText"
-            placeholder="Tell your story..."
-            type="text"
-            autoFocus={true}
-          />
-        </div>
-        <button className="writeSubmit" >
-          Publish
-        </button>
-      </form>
-    </div>
-  );
-=======
 import { useContext, useState } from "react";
 import axios from "axios";
 import { Context } from "../../context/Context";
+import NavBar from "../../components/navbar/NavBar";
 
 export default function Write() {
-    const [title, setTitle] = useState("");
-    const [desc, setDesc] = useState("");
-    const [file, setFile] = useState(null);
-    const { user } = useContext(Context);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const newPost = {
-            username: user.username,
-            title,
-            desc,
-        };
-        if (file) {
-            const data = new FormData();
-            const filename = Date.now() + file.name;
-            data.append("name", filename);
-            data.append("file", file);
-            newPost.photo = filename;
-            try {
-                await axios.post("/upload", data);
-            } catch (err) { }
-        }
-        try {
-            const res = await axios.post("/posts", newPost);
-            window.location.replace("/post/" + res.data._id);
-        } catch (err) { }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      desc,
     };
-
-    return (
-        <div className="pt-12">
-            <img
-                className="ml-36 w-[80vw] h-80 rounded-xl object-cover"
-                src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                alt=""
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newPost.photo = filename;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) { }
+    }
+    try {
+      const res = await axios.post("/posts", newPost);
+      window.location.replace("/post/" + res.data._id);
+    } catch (err) { }
+  };
+  return (
+    <>
+      <NavBar />
+      <div className="pt-12">
+        {file && (
+          <img className="ml-36 w-[80vw] h-80 rounded-xl object-cover" src={URL.createObjectURL(file)} alt="" />
+        )}
+        <form className="relative" onSubmit={handleSubmit}>
+          <div className="flex ml-36 item-center">
+            <label htmlFor="fileInput">
+              <i className="w-8 h-8 flex items-center justify-center text-xl text-white cursor-pointer mt-[22px] fa-regular fa-image"></i>
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
             />
-            <form className="relative">
-                <div className="flex ml-36 item-center">
-                    <label htmlFor="fileInput">
-                        <i className="writeIcon fas fa-plus"></i>
-                    </label>
-                    <input id="fileInput" type="file" style={{ display: "none" }} />
-                    <input
-                        className="writeInput bg-biru-abu"
-                        placeholder="Title"
-                        type="text"
-                        autoFocus={true}
-                    />
-                </div>
-                <div className="flex ml-36 item-center">
-                    <textarea
-                        className="writeInput writeText bg-biru-abu"
-                        placeholder="Tell your story..."
-                        type="text"
-                        autoFocus={true}
-                    />
-                </div>
-                <button className="writeSubmit" type="submit">
-                    Publish
-                </button>
-            </form>
-        </div>
-    );
->>>>>>> upstream/main
+            <input
+              type="text"
+              placeholder="Title"
+              className="text-3xl border-none p-5 w-[70vw] focus:outline-none bg-biru-abu"
+              autoFocus={true}
+              onChange={e => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="flex ml-36 item-center">
+            <textarea
+              placeholder="Tell your story..."
+              type="text"
+              className="text-xl h-[100vw] border-none p-5 w-[70vw] focus:outline-none bg-biru-abu"
+              onChange={e => setDesc(e.target.value)}
+            ></textarea>
+          </div>
+          <button className="absolute top-5 right-[150px] text-white bg-biru-tombol p-[10px] border-none cursor-pointer text-base rounded-md hover:bg-kuning hover:text-black" type="submit">
+            Publish
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
